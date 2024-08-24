d4dd9930441e87d8b9512e5e24d1fc42825c12f8
khesir <khesir2002@gmail.com>
2024-08-25 01:30:20 +0800

    fix: bug on mainlayout that causes to overflow fix

--- src/layout/project-layout.tsx
+++ src/layout/project-layout.tsx
@@ -19,23 +19,25 @@ export type SidebarItems = {
 
 export function ProjectLayout({layout}: SidebarItems) {
 	return (
-		<div className="w-full h-screen flex bg-slate-600">
-			{/* Side bar */}
-			<Sidebar layout={layout} />
-			{/* Menu bar */}
-			<div className="flex flex-col w-full">
-				<header className="flex justify-between items-center bg-slate-700 h-[49px] px-3 text-white border-b border-black shadow-sm">
-					{/* Navbar */}
-					<Navbar />
-					<div className="flex gap-5">
-						<QuickAccessIcons />
+		<div className="w-full h-screen bg-slate-600">
+			<div className='flex h-full'>
+				{/* Side bar */}
+				<Sidebar layout={layout} />
+				{/* Menu bar */}
+				<div className="flex flex-col w-full h-full">
+					<header className="flex justify-between items-center bg-slate-700 h-[49px] px-3 text-white border-b border-black shadow-sm">
+						{/* Navbar */}
+						<Navbar />
+						<div className="flex gap-5">
+							<QuickAccessIcons />
+						</div>
+					</header>
+					{/* Main Conrtent */}
+					<div className="overflow-y-auto h-full">
+						<Outlet />
 					</div>
-				</header>
-				{/* Main Conrtent */}
-				<div className="w-full flex-1 overflow-y-auto">
-					<Outlet />
+					<Toaster />
 				</div>
-				<Toaster />
 			</div>
 		</div>
 	);
